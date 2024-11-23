4c0f354d3cb040785b517fc9027047a0145aecb1
GabrielRibeiro1503 <gabrielribeiro1503@edu.unifil.br>
2024-11-23 16:56:02 -0300

    Recomeçando

--- config/testConnection.js
+++
@@ -1,9 +0,0 @@
-const sequelize = require('./database');
-
-sequelize.authenticate()
-    .then(() => {
-        console.log('Conexão com o banco de dados realizada com sucesso!');
-    })
-    .catch((err) => {
-        console.error('Não foi possível conectar ao banco de dados:', err);
-    });
